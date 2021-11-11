31df3b9ef40804a6e9079ea07aa00f8945bee6b1
9908229348 <kalavaisravanthi98@gmail.com>
2021-11-11 11:59:47 +0530

    Refactored: Test method to check whether InvoiceGenerator returns InvoiceSummary or not

--- test/java/InvoiceServiceTest.java
+++ test/java/InvoiceServiceTest.java
@@ -1,10 +1,16 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+    InvoiceGenerator invoiceGenerator;
+
+    @Before
+    public void setUp() {
+        invoiceGenerator = new InvoiceGenerator();
+    }
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
@@ -13,7 +19,6 @@ public class InvoiceServiceTest {
 
     @Test
     public void givenLessDistanceorTime_ShouldReturnMinFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
@@ -21,13 +26,13 @@ public class InvoiceServiceTest {
     }
 
     @Test
-    public void givenMultipleRides_ShouldReturnTotalFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+    public void givenMultipleRides_ShouldReturnInvoiceSummary(){
         Ride[] rides = { new Ride(0.5, 5),
                          new Ride(0.1, 1),
                          new Ride(2.5, 10)
                         };
-        double fare = invoiceGenerator.calculateFare(rides);
-        Assert.assertEquals(50, fare, 0.0);
+        InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(3, 50.0);
+        Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
     }
 }
